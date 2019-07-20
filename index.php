ac81fced5d57b041a0e44d82e7d6d45c986214f0
Marcelo Diament <marcelodiament@gmail.com>
2019-07-20 15:29:22 -0300

    atualizações meta head

--- index.php
+++ index.php
@@ -4,6 +4,45 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Fita K7 Player | JS</title>
+
+	<!-- META DATA -->
+    <meta name="identifier-URL" content="https://djament.com.br/estudos/js-fitak7/index.php">
+    <meta name="url" content="https://djament.com.br/estudos/js-fitak7/index.php">
+    <meta name="abstract" content="Fita K7 Player">
+    <meta name="author" content="Marcelo Diament, Djament Comunicação">
+    <meta name="robots" content="index,follow">
+    <meta name="contact" content="contato@djament.com.br">
+    <meta name="reply-to" content="contato@djament.com.br">
+    <meta name="copyright" content="Djament">
+    <meta name="rating" content="general">
+    <meta name="web_author" content="Djament Comunicação"> 
+    <!-- META OG -->
+    <meta property="og:site_name" content="Fita K7 Player">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Fita K7 Player">
+    <meta property="og:description" content="Player em JavaScript simulando Toca Fitas (Fita K7)">
+    <meta property="og:url" content="https://djament.com.br/estudos/js-fitak7/index.php">
+    <meta property="og:locale" content="pt-BR">
+    <meta name="og:locality" content="São Paulo">
+    <meta name="og:region" content="SP">
+    <meta name="og:country-name" content="BR">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://djament.com.br/estudos/js-fitak7/assets/img/k7.png">
+    <meta property="og:image:alt" content="Fita K7 Player">
+    <meta property="og:image:url" content="https://djament.com.br/estudos/js-fitak7/assets/img/k7.png">
+    <meta property="og:image:secure_url" content="https://djament.com.br/estudos/js-fitak7/assets/img/k7.png">
+    <meta property="og:image:type" content="img/png">
+    <meta property="og:image:width" content="581">
+    <meta property="og:image:height" content="334">
+    <!-- MOBILE SPECS -->
+    <meta name="application-name" content="JS | Fita K7 Player">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Fita K7 Player"> 
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="msapplication-TileColor" content="#039FCE">
+    <meta name="theme-color" content="#039FCE">
+	<link rel="icon" sizes="581x334" href="https://djament.com.br/estudos/js-fitak7/assets/img/k7.png">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-vlOMx0hKjUCl4WzuhIhSNZSm2yQCaf0mOU1hEDK/iztH3gU4v5NMmJln9273A6Jz" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Caveat|Darker+Grotesque&display=swap" rel="stylesheet">
@@ -14,7 +53,7 @@
 	<main class="container">
 		<section id="intro" class="row">
 			<article class="col-12">
-				<h1>JS | Player Fita K7</h1>
+				<h1>JS | Player Fita K7 <a href="https://github.com/Marcelo-Diament/js-fitak7" title="repositório" rel="noreferrer"><i class="fab fa-github"></i></a></h1>
 				<p>Player de música simulando um toca fitas (fitaK7) feito em JS</p>
 				<p>Por favor, acesse de um desktop (versão beta para estudos de JS)</p>
 			</article>
